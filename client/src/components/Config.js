6bef0cb7b8edf4e9c696d25e2600fa6ac82a4fc8
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2022-03-18 18:34:39 +0900

    LandingPage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '9d43209ee7778c0c911e7f36b9330e6f';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,76 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
+import GridCards from '../commons/GridCards';
+import MainImage from './Sections/MainImage';
+import { Row } from 'antd';
 
 function LandingPage() {
+    const [movies, setMovies] = useState([]);
+    const [mainMovieImage, setMainMovieImage] = useState();
+    const [currentPage, setCurrentPage] = useState(0);
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
+
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([...movies, ...response.results]);
+                setMainMovieImage(response.results[0]);
+                setCurrentPage(response.page);
+            });
+    }, [])
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=${currentPage + 1}`;
+
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([...movies, ...response.results]);
+                setCurrentPage(response.page);
+            });
+    }
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+            
+            {/* Main Image */}
+            {
+                mainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${mainMovieImage.backdrop_path}`}
+                    title={mainMovieImage.original_title}
+                    text={mainMovieImage.overview}
+                />
+            }
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards */}
+                <Row gutter={[16, 16]}>
+                    {
+                        movies && movies.map((movie, index) => (
+                            <React.Fragment key={index}>
+                                <GridCards
+                                    image={movie.poster_path ?
+                                        `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                    movieId={movie.id}
+                                    movieName={movie.original_title}
+                                />
+                            </React.Fragment>
+                        ))
+                    }
+                </Row>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button onClick={loadMoreItems}>Load More</button>
+            </div>
+
+        </div>
     )
 }
 
